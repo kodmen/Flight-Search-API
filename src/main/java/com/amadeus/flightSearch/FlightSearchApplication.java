b4228147f79efb03e27d5ec947dc0ef709e38d53
bedirhan ergün <bedirhan1029@gmail.com>
2023-12-28 13:40:01 +0300

    swagger added

--- src/main/java/com/amadeus/flightSearch/FlightSearchApplication.java
+++ src/main/java/com/amadeus/flightSearch/FlightSearchApplication.java
@@ -2,11 +2,14 @@ package com.amadeus.flightSearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.amadeus.*")
 public class FlightSearchApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/springboot3");
 		SpringApplication.run(FlightSearchApplication.class, args);
 	}
 
