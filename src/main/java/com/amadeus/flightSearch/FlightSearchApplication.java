f8b4ed935153217df3c07d612fffd477b9f0b08c
bedirhan ergün <bedirhan1029@gmail.com>
2023-12-29 09:52:15 +0300

    FlihghtSearchApplication update

--- src/main/java/com/amadeus/flightSearch/FlightSearchApplication.java
+++ src/main/java/com/amadeus/flightSearch/FlightSearchApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.amadeus.*")
 public class FlightSearchApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("server.servlet.context-path", "/springboot3");
 		SpringApplication.run(FlightSearchApplication.class, args);
 	}
 
