4225075cf2ea5a2b0967c5a8fc63c1a34a64a6f4
bedirhan ergün <bedirhan1029@gmail.com>
2023-12-29 09:53:39 +0300

    exception handler added

---
+++ src/main/java/com/amadeus/flightSearch/advisers/AppWideExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.amadeus.flightSearch.advisers;
+
+
+import com.amadeus.flightSearch.exception.NotFoundException;
+import com.amadeus.flightSearch.exception.ValidateException;
+import com.amadeus.flightSearch.util.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleException(Exception e) {
+        return new ResponseEntity(new StandardResponse("500", "Error", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException(NotFoundException e) {
+        return new ResponseEntity(new StandardResponse("404", "Error", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidateException.class)
+    public ResponseEntity handleValidationException(ValidateException e) {
+        return new ResponseEntity(new StandardResponse("400", "Error", e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
