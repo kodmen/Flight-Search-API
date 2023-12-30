f529ddc3eccdb535ea42497efaf9193c6e0ddf23
bedirhan ergün <bedirhan1029@gmail.com>
2023-12-30 21:25:40 +0300

    swagger config update

--- src/main/java/com/amadeus/flightSearch/FlightSearchApplication.java
+++ src/main/java/com/amadeus/flightSearch/FlightSearchApplication.java
@@ -1,11 +1,19 @@
 package com.amadeus.flightSearch;
 
+import com.amadeus.flightSearch.entity.Flight;
+import com.amadeus.flightSearch.service.FlightService;
+import com.amadeus.flightSearch.util.FlightUtil;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
-public class FlightSearchApplication {
+@EnableScheduling
+public class FlightSearchApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightSearchApplication.class, args);
--- src/main/java/com/amadeus/flightSearch/config/OpenAPIConfig.java
+++ src/main/java/com/amadeus/flightSearch/config/OpenAPIConfig.java
@@ -30,27 +30,27 @@ public class OpenAPIConfig {
 
     @Bean
     public OpenAPI myOpenAPI() {
-        Server devServer = new Server();
-        devServer.setUrl(devUrl);
-        devServer.setDescription("Server URL in Development environment");
-
-        Server prodServer = new Server();
-        prodServer.setUrl(prodUrl);
-        prodServer.setDescription("Server URL in Production environment");
-
-        Contact contact = new Contact();
-        contact.setEmail("contact@pasanabeysekara.com");
-        contact.setName("Pasan Abeysekara");
-        contact.setUrl("https://www.pasanabeysekara.com");
-
-        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
-
-        Info info = new Info()
-                .title("Demo Service API")
-                .version("1.0")
-                .contact(contact)
-                .description("This API exposes endpoints to manage demo.").termsOfService("https://www.pasanabeysekara.com")
-                .license(mitLicense);
+//        Server devServer = new Server();
+//        devServer.setUrl(devUrl);
+//        devServer.setDescription("Server URL in Development environment");
+//
+//        Server prodServer = new Server();
+//        prodServer.setUrl(prodUrl);
+//        prodServer.setDescription("Server URL in Production environment");
+//
+//        Contact contact = new Contact();
+//        contact.setEmail("contact@pasanabeysekara.com");
+//        contact.setName("Pasan Abeysekara");
+//        contact.setUrl("https://www.pasanabeysekara.com");
+//
+//        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+//
+//        Info info = new Info()
+//                .title("Demo Service API")
+//                .version("1.0")
+//                .contact(contact)
+//                .description("This API exposes endpoints to manage demo.").termsOfService("https://www.pasanabeysekara.com")
+//                .license(mitLicense);
 
 //        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
         final String securitySchemeName = "bearerAuth";
